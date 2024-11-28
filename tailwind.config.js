e2963261a3a998243c7f97e6806482277d0f76c1
Mathieu <gaucher.mathieu@icloud.com>
2024-11-28 14:19:54 +0100

    mise en place architecture + add PWA

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',          // Inclut le fichier HTML principal
+    './src/**/*.{js,ts}',    // Inclut tous les fichiers JS/TS dans le répertoire src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      manifest: {
+        name: 'Ma PWA',
+        short_name: 'PWA',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,json}']
+      }
+    })
+  ]
+});
